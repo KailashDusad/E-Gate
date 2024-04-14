69e9ad3750c8862f6f88a1d18e33d54a56af6643
KailashDusad <kailash.dusad@iitgn.ac.in>
2024-04-14 05:51:04 +0530

    Continue With Google For Resident users

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
 const app = express();
 const cors = require('cors');
-const {User} = require('./config');
+const { User } = require('./config');
 const admin = require('firebase-admin');
 const qr = require('qrcode');
 const fs = require('fs');
-const router = express.Router();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// Check if Firebase Admin SDK is not already initialized
+if (!admin.apps.length) {
+    // Initialize Firebase Admin SDK
+    const serviceAccount = require('./egate-27caa-firebase-adminsdk-lrl0e-50bd2f82a5.json');
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+}
+
+// Configure Passport.js
+passport.use(new GoogleStrategy({
+    clientID: '757561340490-qmsjjo78o48i9ll4n1thagp5hdg2a58h.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: 'http://localhost:1304/auth/google/callback',
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+    scope: ['email', 'profile']
+},
+function(accessToken, refreshToken, profile, cb) {
+    // This function will be called after successful authentication
+    return cb(null, profile);
+}
+));
+app.use(session({
+    secret: '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',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Serialize user
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+// Deserialize user
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 app.set('view engine', 'pug');
@@ -14,174 +56,221 @@ app.set('views', path.join(__dirname, '/static/pug'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+// Routes
 app.get('/', (req, res) => {
-    res.status(200).render('home.pug');
+  res.status(200).render('home.pug');
 });
 
 app.get('/permanent', (req, res) => {
-    res.status(200).render('permanent.pug');
+  res.status(200).render('permanent.pug');
 });
+
 app.get('/visitors', (req, res) => {
-    res.status(200).render('visitors.pug');
+  res.status(200).render('visitors.pug');
 });
+
 app.get('/security', (req, res) => {
-    res.status(200).render('security.pug');
-});
-router.post('/permanent/googleSignIn', async (req, res) => {
-    try {
-        const idToken = req.body.idToken;
-        const decodedToken = await admin.auth().verifyIdToken(idToken);
-        const email = decodedToken.email;
-        
-        // Save user's Gmail ID to Firebase
-        await User.doc(email).set({ email });
-        
-        // Generate QR code
-        const qrData = {
-            email,
-            signInTime: new Date().toISOString()
-        };
-        const qrString = JSON.stringify(qrData);
-        await qr.toFile("user.png", qrString);
-
-        res.json({ message: 'User signed in successfully' });
-    } catch (error) {
-        console.error('Error signing in with Google:', error);
-        res.status(500).json({ error: 'An error occurred while signing in with Google' });
-    }
+  res.status(200).render('security.pug');
 });
 
-module.exports = router;
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+
+  async function(req, res) {
+    // const userG = req.user;
+    // if (userG.email.endsWith('@iitgn.ac.in')) {
+    //     // User.create({ email: user.email, googleId: user.id })
+    //     const userResponseG = await admin.auth().createUser({
+    //         email: userG.email,
+    //         googleId: userG.id,
+    //         emailVerified: false,
+    //         disabled: false,
+    //       });
+      
+    //       const dbG = admin.firestore();
+    //       const visitorsG = dbG.collection('users').doc(userResponseG.uid);
+    //       visitorsG.get().then((docSnapshot) => {
+    //         if (!docSnapshot.exists) {
+    //           visitorsG.set({
+    //             email: userG.email,
+    //             uid: userResponse.uid,
+    //           }).catch((error) => {
+    //             console.log("Error setting document:", error);
+    //           });
+    //         } else {
+    //           visitorsG.update({
+    //             email: userG.email,
+    //             uid: userResponse.uid,
+    //           }).catch((error) => {
+    //             console.log("Error updating document:", error);
+    //           });
+    //         }
+    //       }).catch((error) => {
+    //         console.log("Error getting document:", error);
+    //       });
+    //       let qrString = JSON.stringify({ email: userG.email, uid: userResponse.uid });
+
+    //     await qr.toFile("userG.png", qrString);
+    //     const sourcePathG = 'userG.png';
+    //     const destinationPathG = path.join(__dirname, 'static', 'userG.png');
+
+    //     fs.copyFile(sourcePathG, destinationPathG, (err) => {
+    //     if (err) {
+    //         console.error('Error copying file:', err);
+    //     } else {
+    //         console.log('File copied successfully');
+    //     }
+    //     });
+    //     res.redirect('/permanent');
+
+    // } else {
+    //     res.render('popup.pug', { message: 'Only IIT GN students are allowed.' });
+    //   }
+
+    res.redirect('/permanent');
+  }
+);
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
 app.post('/permanent', async (req, res) => {
-    
-    try {
-        const data = req.body;
+  try {
+    const data = req.body;
 
-        if (!data.email.endsWith('@iitgn.ac.in')) {
-            return res.status(400).json({ error: 'Invalid email domain. Only @iitgn.ac.in is allowed.' });
-        }
-
-        const userResponse = await admin.auth().createUser({
-            email: data.email,
-            password: data.password,
-            emailVerified: false,
-            disabled: false,
-        });
-
-        const db = admin.firestore();
-        const visitors = db.collection('users').doc(userResponse.uid);
-
-        visitors.get().then((docSnapshot) => {
-            if (!docSnapshot.exists) {
-                visitors.set({
-                    email: data.email,
-                    uid: userResponse.uid,
-                }).catch((error) => {
-                    console.log("Error setting document:", error);
-                });
-            } else {
-                visitors.update({
-                    email: data.email,
-                    uid: userResponse.uid,
-                }).catch((error) => {
-                    console.log("Error updating document:", error);
-                });
-            }
-        }).catch((error) => {
-            console.log("Error getting document:", error);
-        });
-
-        let qrString = JSON.stringify({email: data.email, uid: userResponse.uid});
-            
-            await qr.toFile("user.png", qrString);
-            const sourcePath = 'user.png';
-            const destinationPath = path.join(__dirname, 'static', 'user.png');
-
-            fs.copyFile(sourcePath, destinationPath, (err) => {
-                if (err) {
-                    console.error('Error copying file:', err);
-                    // Handle the error
-                } else {
-                    console.log('File copied successfully');
-                    // Continue with your code
-                }
-            });
-            res.render('permanent.pug', { message: 'User created successfully.', qrImage: 'user.png' });
-
-
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'An error occurred while creating the user.', details: error.message });
+    if (!data.email.endsWith('@iitgn.ac.in')) {
+    //   return res.render('permanent.pug', { external: 'Invalid email domain. Only @iitgn.ac.in is allowed.' });
+    return res.json({ error: 'Invalid email domain. Only @iitgn.ac.in is allowed.' });
     }
-    
+
+    const userResponse = await admin.auth().createUser({
+      email: data.email,
+      password: data.password,
+      emailVerified: false,
+      disabled: false,
+    });
+
+    const db = admin.firestore();
+    const visitors = db.collection('users').doc(userResponse.uid);
+
+    visitors.get().then((docSnapshot) => {
+      if (!docSnapshot.exists) {
+        visitors.set({
+          email: data.email,
+          uid: userResponse.uid,
+        }).catch((error) => {
+          console.log("Error setting document:", error);
+        });
+      } else {
+        visitors.update({
+          email: data.email,
+          uid: userResponse.uid,
+        }).catch((error) => {
+          console.log("Error updating document:", error);
+        });
+      }
+    }).catch((error) => {
+      console.log("Error getting document:", error);
+    });
+
+    let qrString = JSON.stringify({ email: data.email, uid: userResponse.uid });
+
+    await qr.toFile("user.png", qrString);
+    const sourcePath = 'user.png';
+    const destinationPath = path.join(__dirname, 'static', 'user.png');
+
+    fs.copyFile(sourcePath, destinationPath, (err) => {
+      if (err) {
+        console.error('Error copying file:', err);
+      } else {
+        console.log('File copied successfully');
+      }
+    });
+
+    res.render('permanent.pug', { message: 'User created successfully.', qrImage: 'user.png' });
+
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while creating the user.', details: error.message });
+  }
 });
+
 app.post('/visitors', async (req, res) => {
-    try {
-        const data = req.body;
-        const userResponse = await admin.auth().createUser({
-            email: data.email,
-            name: data.name,
-            phone: data.phone,
-            intime: data.intime,
-            emailVerified: false,
-            disabled: false,
-        });
+  try {
+    const data = req.body;
+    const userResponse = await admin.auth().createUser({
+      email: data.email,
+      name: data.name,
+      phone: data.phone,
+      intime: data.intime,
+      emailVerified: false,
+      disabled: false,
+    });
 
-        const db = admin.firestore();
-        const visitors = db.collection('visitors').doc(userResponse.uid);
+    const db = admin.firestore();
+    const visitors = db.collection('visitors').doc(userResponse.uid);
 
-        visitors.get().then((docSnapshot) => {
-            if (!docSnapshot.exists) {
-                visitors.set({
-                    email: data.email,
-                    name: data.name,
-                    phone: data.phone,
-                    intime: data.intime,
-                    uid: userResponse.uid,
-                }).catch((error) => {
-                    console.log("Error setting document:", error);
-                });
-            } else {
-                visitors.update({
-                    email: data.email,
-                    name: data.name,
-                    phone: data.phone,
-                    intime: data.intime,
-                    uid: userResponse.uid,
-                }).catch((error) => {
-                    console.log("Error updating document:", error);
-                });
-            }
+    visitors.get().then((docSnapshot) => {
+      if (!docSnapshot.exists) {
+        visitors.set({
+          email: data.email,
+          name: data.name,
+          phone: data.phone,
+          intime: data.intime,
+          uid: userResponse.uid,
         }).catch((error) => {
-            console.log("Error getting document:", error);
+          console.log("Error setting document:", error);
         });
+      } else {
+        visitors.update({
+          email: data.email,
+          name: data.name,
+          phone: data.phone,
+          intime: data.intime,
+          uid: userResponse.uid,
+        }).catch((error) => {
+          console.log("Error updating document:", error);
+        });
+      }
+    }).catch((error) => {
+      console.log("Error getting document:", error);
+    });
 
-        let qrString = JSON.stringify({email: data.email, name: data.name, phone: data.phone, intime: data.intime});
-            
-            await qr.toFile("visitors.png", qrString);
-            const sourcePath = 'visitors.png';
-            const destinationPath = path.join(__dirname, 'static', 'visitors.png');
+    let qrString = JSON.stringify({ email: data.email, name: data.name, phone: data.phone, intime: data.intime });
 
-            fs.copyFile(sourcePath, destinationPath, (err) => {
-                if (err) {
-                    console.error('Error copying file:', err);
-                    // Handle the error
-                } else {
-                    console.log('File copied successfully');
-                    // Continue with your code
-                }
-            });
-            res.render('visitors.pug', { message: 'User created successfully.'});
+    await qr.toFile("visitors.png", qrString);
+    const sourcePath = 'visitors.png';
+    const destinationPath = path.join(__dirname, 'static', 'visitors.png');
 
+    fs.copyFile(sourcePath, destinationPath, (err) => {
+      if (err) {
+        console.error('Error copying file:', err);
+      } else {
+        console.log('File copied successfully');
+      }
+    });
 
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'An error occurred while creating the user.', details: error.message });
-    }
-    
+    res.render('visitors.pug', { message: 'User created successfully.' });
+
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while creating the user.', details: error.message });
+  }
+
 });
 
 app.listen(process.env.PORT || 1304, () => {
-    console.log('running on port 1304');
+  console.log('running on port 1304');
 });
+
+
+
+
+// clientID: '757561340490-qmsjjo78o48i9ll4n1thagp5hdg2a58h.apps.googleusercontent.com',
+//     clientSecret: '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',
